94664fe804b5bafc18ff3e8544064df589a4ed1e
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-22 16:58:08 -0500

    style component

--- App.js
+++ App.js
@@ -27,8 +27,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
     backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
   }
 });
