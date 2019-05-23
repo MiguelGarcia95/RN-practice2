39af7ff1e7afb747f6ddd8a1076e76ae633f414f
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-22 20:21:03 -0500

    work on list and input component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 import List from './src/components/List';
 import InputContainer from './src/components/InputContainer';
