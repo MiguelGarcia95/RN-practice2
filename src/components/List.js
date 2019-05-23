3f38959d45f5482c00294d16e99de7f6d13da575
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-22 19:37:01 -0500

    work on list and input component

--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-const List = () => {
+const List = ({places}) => {
+  const places = this.state.places.map(place => <ListItem key={place} placeName={place} />)
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.listContainer}>
+      {places}
     </View>
   )
 }
