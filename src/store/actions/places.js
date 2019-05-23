0386159920dbdf0426595c25a9d51ca42e19ec37
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-23 13:49:41 -0500

    add action types

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,6 +2,31 @@ import * as actionTypes from './actionTypes';
 
 export const addPlace = placeName => {
   return {
-
+    type: actionTypes.ADD_PLACE,
+    payload: {
+      placeName: placeName
+    }
   };
 }
+
+export const deletePlace = () => {
+  return {
+    type: actionTypes.DELETE_PLACE
+  };
+}
+
+export const selectPlace = key => {
+  return {
+    type: actionTypes.SELECT_PLACE,
+    payload: {
+      placeKey: key
+    }
+  };
+}
+
+export const unselectPlace = () => {
+  return {
+    type: actionTypes.UNSELECT_PLACE
+  };
+}
+
