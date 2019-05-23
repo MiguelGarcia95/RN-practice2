db00206f417d1a9a1795bcffa61faa042e382790
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-23 13:46:14 -0500

    add action types

---
+++ src/store/reducers/places.js
@@ -0,0 +1,17 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case value:
+      break;
+    default:
+      return state;
+  }
+}
+
+export default reducer;
