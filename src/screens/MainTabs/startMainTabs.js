7d1aaa25cfeacfce266b5808030fd0178656aaca
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 15:01:12 -0500

    added tab icons

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,19 +1,27 @@
 import {Navigation} from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: "places.FindPlaceScreen",
-        label: 'Find Place',
-        title: 'Find Place'
-      },
-      {
-        screen: "places.SharePlaceScreen",
-        label: 'Share Place',
-        title: 'Share Place'
-      },
-    ]
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('md-share-alt', 30),
+  ]).then(sources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: "places.FindPlaceScreen",
+          label: 'Find Place',
+          title: 'Find Place',
+          icon: sources[0],
+        },
+        {
+          screen: "places.SharePlaceScreen",
+          label: 'Share Place',
+          title: 'Share Place',
+          icon: sources[1],
+        },
+      ]
+    });
   });
 }
 
