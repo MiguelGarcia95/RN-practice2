2f6b0418744a7b43fb6c2ca688082e0b2f201d3c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 12:29:22 -0500

    add screens

--- App.js
+++ App.js
@@ -1,84 +1,86 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {connect} from 'react-redux';
+// import React, {Component} from 'react';
+// import {StyleSheet, View} from 'react-native';
+// import {connect} from 'react-redux';
 
-import List from './src/components/List';
-import InputContainer from './src/components/InputContainer';
-import PlaceModal from './src/components/PlaceModal';
-import {addPlace, deletePlace, selectPlace, unselectPlace} from './src/store/actions';
+// import List from './src/components/List';
+// import InputContainer from './src/components/InputContainer';
+// import PlaceModal from './src/components/PlaceModal';
+// import {addPlace, deletePlace, selectPlace, unselectPlace} from './src/store/actions';
 
-class App extends Component {
-  state = {
-    placeName: ''
-  }
+// class App extends Component {
+//   state = {
+//     placeName: ''
+//   }
 
-  placeNameChangedHandler = (event) => {
-    this.setState({placeName: event});
-  }
+//   placeNameChangedHandler = (event) => {
+//     this.setState({placeName: event});
+//   }
 
-  placeAddedHandler = () => {
-    if (this.state.placeName) {
-      this.props.onAddPlace(this.state.placeName);
-      this.setState({placeName: ''});
-    } else {
-      return;
-    }
-  }
+//   placeAddedHandler = () => {
+//     if (this.state.placeName) {
+//       this.props.onAddPlace(this.state.placeName);
+//       this.setState({placeName: ''});
+//     } else {
+//       return;
+//     }
+//   }
 
-  placeSelectedHandler = key => this.props.onPlaceSelect(key);
+//   placeSelectedHandler = key => this.props.onPlaceSelect(key);
 
-  placeDeleteHandler = () => this.props.onPlaceDelete();
+//   placeDeleteHandler = () => this.props.onPlaceDelete();
 
-  placeUnselectedHandler = () => this.props.onPlaceUnselect();
-
-
-
-  render() {
-    const {placeName} = this.state;
-    const {selectedPlace, places} = this.props;
-    return (
-      <View style={styles.container}>
-        <PlaceModal 
-          place={selectedPlace} 
-          onModalClose={this.placeUnselectedHandler} 
-          onItemDelete={this.placeDeleteHandler} 
-        />
-        <InputContainer 
-          placeName={placeName} 
-          placeNameChangedHandler={this.placeNameChangedHandler}  
-          placeAddedHandler={this.placeAddedHandler}
-        />
-        <List places={places} onItemSelected={this.placeSelectedHandler} />
-      </View>
-    );
-  }
-}
+//   placeUnselectedHandler = () => this.props.onPlaceUnselect();
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#F5FCFF',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-});
 
-const mapStateToProps = state => {
-  return {
-    places: state.places.places,
-    selectedPlace: state.places.selectedPlace
-  }
-}
+//   render() {
+//     const {placeName} = this.state;
+//     const {selectedPlace, places} = this.props;
+//     return (
+//       <View style={styles.container}>
+//         <PlaceModal 
+//           place={selectedPlace} 
+//           onModalClose={this.placeUnselectedHandler} 
+//           onItemDelete={this.placeDeleteHandler} 
+//         />
+//         <InputContainer 
+//           placeName={placeName} 
+//           placeNameChangedHandler={this.placeNameChangedHandler}  
+//           placeAddedHandler={this.placeAddedHandler}
+//         />
+//         <List places={places} onItemSelected={this.placeSelectedHandler} />
+//       </View>
+//     );
+//   }
+// }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    onAddPlace: (placeName) => dispatch(addPlace(placeName)),
-    onPlaceDelete: () => dispatch(deletePlace()),
-    onPlaceSelect: (key) => dispatch(selectPlace(key)),
-    onPlaceUnselect: () => dispatch(unselectPlace()),
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     padding: 20,
+//     backgroundColor: '#F5FCFF',
+//     justifyContent: 'flex-start',
+//     alignItems: 'center',
+//   },
+// });
+
+// const mapStateToProps = state => {
+//   return {
+//     places: state.places.places,
+//     selectedPlace: state.places.selectedPlace
+//   }
+// }
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     onAddPlace: (placeName) => dispatch(addPlace(placeName)),
+//     onPlaceDelete: () => dispatch(deletePlace()),
+//     onPlaceSelect: (key) => dispatch(selectPlace(key)),
+//     onPlaceUnselect: () => dispatch(unselectPlace()),
+//   }
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import React from 'react';
 import {AppRegistry} from 'react-native';
-import {Provider} from 'react-redux';
+// import {Provider} from 'react-redux';
 import App from './App';
-import configureStore from './src/store/configureStore';
 import {name as appName} from './app.json';
 
-const store = configureStore();
+// import configureStore from './src/store/configureStore';
 
-const AppWithRedux = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider> 
-  )
-}
+// const store = configureStore();
 
-AppRegistry.registerComponent(appName, () => AppWithRedux);
+// const AppWithRedux = () => {
+//   return (
+//     <Provider store={store}>
+//       <App />
+//     </Provider> 
+//   )
+// }
+
+AppRegistry.registerComponent(appName, () => App);
