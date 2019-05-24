d55a6531cb89ebbf151ba6c9e05f77115be785ca
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 18:32:01 -0500

    connect screen to redux

--- src/screens/SharePlace/SharePlace.js
+++ src/screens/SharePlace/SharePlace.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
+import InputContainer from '../../components/InputContainer';
+
 class SharePlaceScreen extends Component {
   render() {
     return(
