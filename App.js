d3a5388c2d0d33fe018410be4a96d0a3f12c49fc
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 14:22:47 -0500

    added screens

--- App.js
+++ App.js
@@ -86,8 +86,12 @@
 import {Navigation} from 'react-native-navigation';
 
 import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 
 Navigation.registerComponent("places.AuthScreen", () => AuthScreen);
+Navigation.registerComponent("places.SharePlaceScreen", () => SharePlaceScreen);
+Navigation.registerComponent("places.FindPlaceScreen", () => FindPlaceScreen);
 
 Navigation.startSingleScreenApp({
   screen: {
