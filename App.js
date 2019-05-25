e77fc6bcb7225cbc116c732f647738a1bbfa0200
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 22:01:30 -0500

    load drawer menu

--- App.js
+++ App.js
@@ -68,6 +68,7 @@ import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import SideDrawerScreen from './src/screens/SideDrawer/SideDrawer';
 import configureStore from './src/store/configureStore';
 
 const store = configureStore();
@@ -76,6 +77,7 @@ Navigation.registerComponent("places.AuthScreen", () => AuthScreen, store, Provi
 Navigation.registerComponent("places.SharePlaceScreen", () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent("places.FindPlaceScreen", () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent("places.PlaceDetailScreen", () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent("places.SideDrawerScreen", () => SideDrawerScreen);
 
 
 //Start App 
