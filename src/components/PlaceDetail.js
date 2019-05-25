ba811ccc28bdd81ba80d2cd5edd935b051b05e56
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 20:11:31 -0500

    navigate to place details

--- src/components/PlaceDetail.js
+++ src/components/PlaceDetail.js
@@ -1,31 +1,21 @@
 import React from 'react'
-import { View, Text, Image, Modal, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const PlaceDetail = ({place, onItemDelete, onModalClose}) => {
-  let modalContent = null;
-  if (place) {
-    modalContent = (
-      <View>
-        <Image source={place.image} style={styles.placeImage} />
-        <Text style={styles.placeName} >{place.name}</Text>
-      </View>
-    )
-  }
   return (
-    <Modal onRequestClose={onModalClose} visible={place !== null} animationType='slide' >
-      <View style={styles.modalContainer}> 
-        {modalContent}
-        <View> 
-          <TouchableOpacity onPress={onItemDelete} >
-            <View style={styles.deleteButton}>
-              <Icon size={30} name='ios-trash' color='red' />
-            </View>
-          </TouchableOpacity>
-          <Button title='Close' onPress={onModalClose} /> 
-        </View>
-      </View> 
-    </Modal> 
+    <View style={styles.modalContainer}> 
+      <Image source={place.image} style={styles.placeImage} />
+      <Text style={styles.placeName} >{place.name}</Text>
+      <View> 
+        <TouchableOpacity onPress={onItemDelete} >
+          <View style={styles.deleteButton}>
+            <Icon size={30} name='ios-trash' color='red' />
+          </View>
+        </TouchableOpacity>
+        <Button title='Close' onPress={onModalClose} /> 
+      </View>
+    </View> 
   )
 }
 
