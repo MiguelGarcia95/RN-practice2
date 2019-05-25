e7f5737a4ba9016cb702a7dba739a8b9932cedf1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 23:11:21 -0500

    load drawer menu

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -5,6 +5,7 @@ const startTabs = () => {
   Promise.all([
     Icon.getImageSource('md-map', 30),
     Icon.getImageSource('md-share-alt', 30),
+    Icon.getImageSource('ios-menu', 30),
   ]).then(sources => {
     Navigation.startTabBasedApp({
       tabs: [
@@ -13,12 +14,28 @@ const startTabs = () => {
           label: 'Find Place',
           title: 'Find Place',
           icon: sources[0],
+          navigatorButton: {
+            leftButtons: [
+              {
+                icon: sources[2],
+                title: "Menu"
+              },
+            ]
+          }
         },
         {
           screen: "places.SharePlaceScreen",
           label: 'Share Place',
           title: 'Share Place', 
           icon: sources[1],
+          navigatorButton: {
+            leftButtons: [
+              {
+                icon: sources[2],
+                title: "Menu"
+              },
+            ]
+          }
         },
       ],
       drawer: {
