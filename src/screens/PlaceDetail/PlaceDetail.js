c65cf8202c43fc039a2f7e666226802d2edbc0f5
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 21:10:42 -0500

    pop screen

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,24 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 import {connect} from 'react-redux';
 
 import {deletePlace} from '../../store/actions';
 
-const PlaceDetail = ({selectedPlace, onDeletePlace}) => {
-  return (
-    <View style={styles.container}> 
-      <Image source={selectedPlace.image} style={styles.placeImage} />
-      <Text style={styles.placeName} >{selectedPlace.name}</Text>
-      <View> 
-        <TouchableOpacity onPress={onDeletePlace} >
-          <View style={styles.deleteButton}>
-            <Icon size={30} name='ios-trash' color='red' />
-          </View>
-        </TouchableOpacity>
-      </View>
-    </View> 
-  )
+class PlaceDetail extends Component {
+  render() {
+    return (
+      <View style={styles.container}> 
+        <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+        <Text style={styles.placeName} >{this.props.selectedPlace.name}</Text>
+        <View> 
+          <TouchableOpacity onPress={this.onDeletePlace} >
+            <View style={styles.deleteButton}>
+              <Icon size={30} name='ios-trash' color='red' />
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View> 
+    )
+  }
 }
 
 const styles = StyleSheet.create({
