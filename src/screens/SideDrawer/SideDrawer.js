e527714fefec19546504a5b7fce214594b6a0396
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-24 23:27:00 -0500

    style menu

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text, Dimensions} from 'react-native'
+import {View, Text, Dimensions, StyleSheet} from 'react-native'
 
 class SideDrawer extends Component {
   render() {
