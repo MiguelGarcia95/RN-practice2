dba187ea0b976042f3dc407e20b0107c83ca2a77
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 01:47:24 -0500

    add map to location component

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
-import {View, Button, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet, Dimensions} from 'react-native';
 import MapView from 'react-native-maps';
 
 import MainText from '../UI/MainText/MainText';
 
 class PickLocation extends Component {
+  state = {
+    focusedLocation: {
+      latitude: 37.7900352,
+      longitude: -122.4013726,
+      latitudeDelta: 0.0122,
+      longitudeDelta: 
+        Dimensions.get('window').width / 
+        Dimensions.get('window'.height) * 
+        0.0122,
+    }
+  }
+
   render() {
     return(
       <View style={styles.container}>
         <MapView 
-          initialRegion={}
+          initialRegion={this.state.focusedLocation}
           style={styles.map}
         />
         <View style={styles.button}>
