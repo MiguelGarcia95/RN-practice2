5747a3f3e81fe2cf16540daedf58850a21de9dc9
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 15:15:54 -0500

    add react native image picker and link it

--- android/app/src/main/java/com/practice2/MainActivity.java
+++ android/app/src/main/java/com/practice2/MainActivity.java
@@ -8,12 +8,10 @@ import com.facebook.react.modules.core.PermissionListener;
 public class MainActivity extends SplashActivity {
     private PermissionListener listener; 
 
-      @Override
     public void setPermissionListener(PermissionListener listener) {
         this.listener = listener;
     }
 
-    @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if (listener != null) {
             listener.onRequestPermissionsResult(requestCode, permissions, grantResults);
