a1322a32c3dbb5924c99845dcb49f53dc4dec698
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 03:13:54 -0500

    get current position

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -37,6 +37,23 @@ class PickLocation extends Component {
     })
   }
 
+  getLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const coordsEvent = {
+        nativeEvent: {
+          coordinate: {
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude
+          }
+        }
+      };
+      this.pickLocationHandler(coordsEvent);
+    }, err => {
+      console.log(err);
+      alert('Fetching position failed, pick one manually.');
+    });
+  }
+
   render() {
     let marker = null;
     if (this.state.locationChosen) {
@@ -55,7 +72,7 @@ class PickLocation extends Component {
           {marker}
         </MapView>
         <View style={styles.button}>
-          <Button title='Locate me' onPress={() => alert('pick location')} />
+          <Button title='Locate me' onPress={this.getLocationHandler} />
         </View>
       </View>
     )
