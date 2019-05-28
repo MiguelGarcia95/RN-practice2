eeec6f54a1179c1f022049427f86fd8319bb6316
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 21:31:01 -0500

    add place to server

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,13 +2,32 @@ import * as actionTypes from './actionTypes';
 import {ENTRY_POINT} from '../../../env';
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: actionTypes.ADD_PLACE,
-    payload: {
-      placeName: placeName,
-      location: location,
-      image: image
+  return dispatch => {
+
+    const placeData = {
+      name: placeName,
+      location:  location
     }
+
+    fetch(`${ENTRY_POINT}/places.json`, {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+    .catch(err => console.log(err))
+    .then(res => res.json())
+    .then(parsed => {
+      console.log(parsed);
+    })
+     
+    // dispatch({
+    //   type: actionTypes.ADD_PLACE,
+    //   payload: {
+    //     placeName: placeName,
+    //     location: location,
+    //     image: image
+    //   }
+    // })
+
   };
 }
 
