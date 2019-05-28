b018df66e159bc7a88f89d030498b57b85807afe
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-28 01:10:11 -0500

    fixed key error

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -72,7 +72,7 @@ export const getPlaces = () => {
           image: {
             uri: parsedRes[key].image
           },
-          id: key
+          key: key
         })
       }
       dispatch(setPlaces(places))
