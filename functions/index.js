97730ff405bd4a55e2d585d81017b5b4f7714bdb
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 22:24:48 -0500

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,13 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+
+exports.storeImage = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
+    const body = JSON.parse(request.body);
+  })  
+  response.send("Hello from Firebase!");
 });
