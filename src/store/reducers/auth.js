2097f5ceb54561acc4628e4593ce97b72ca4c4c5
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-28 14:21:30 -0500

    set auth token

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,12 +1,16 @@
 import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
-
+  token: null,
 }
 
 const reducer = (state = initialState, action) {
   switch(action.type) {
     case actionTypes.AUTH_SET_TOKEN:
+      return {
+        ...state,
+        token: action.payload.token
+      }
     default: 
       return state;
   }
