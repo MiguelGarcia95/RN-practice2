931957ff71aa8b834ce2710127e238d632eaec7b
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-27 22:38:11 -0500

    added firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,14 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
+const fs = require('fs');
+const UUID = require('uuid-v4');
+
+const gcconfig = {
+  projectId: 'my-project-rn-te-1558941296674',
+  keyFilename: 'practice_places.json',
+};
+
+const gcs = require('@google-cloud/storage')(gcconfig);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,6 +17,22 @@ const cors = require('cors')({origin: true});
 exports.storeImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     const body = JSON.parse(request.body);
+    fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
+      console.log(err);
+      return response.status(500).json({error: err});
+    });
+    const bucket = gcs.bucket("my-project-rn-te-1558941296674.appspot.com");
+
+    const uuid = UUID();
+
+    bucket.upload("/tmp/uploaded-image.jpg", {
+      uploadType: "media",
+      destination: "/places/" + uuid + ".jpg",
+      metaData: {
+        contentType: "image/jpeg",
+        firebaseStorageDownloadTokens: uuid
+      }
+    });
   })  
   response.send("Hello from Firebase!");
 });
